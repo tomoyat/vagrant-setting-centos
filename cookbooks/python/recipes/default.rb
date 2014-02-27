9cff597696cc4e30003e4c01c1a3caa08d814c03
tomoyat <20t.toya@gmail.com>
2014-02-28 00:37:54 +0900

    added install ansible

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -6,6 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "python-devel" do
+  action :install
+end
+
 cookbook_file "#{Chef::Config[:file_cache_path]}/get-pip.py" do
   source 'get-pip.py'
   mode "0644"
@@ -18,4 +22,11 @@ execute "install-pip" do
     /usr/bin/python get-pip.py
   EOF
   not_if { ::File.exists?("/usr/bin/pip") }
+end
+
+execute "install-ansible" do
+  command <<-EOF
+    sudo /usr/bin/pip install ansible
+  EOF
+  only_if { ::File.exists?("/usr/bin/pip") }
 end
