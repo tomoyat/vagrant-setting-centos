221aa444bc4f11bc560aaa8b134cf267dbafaf2d
tomoyat <20t.toya@gmail.com>
2014-06-07 17:44:03 +0900

    add java cookbooks

---
+++ cookbooks/java/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: java
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "java-1.7.0-openjdk-devel" do
+  action :install
+end
